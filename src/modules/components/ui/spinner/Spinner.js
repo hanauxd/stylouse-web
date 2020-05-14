9392bb785598c5dc1ab2820c6900de9fd9b86850
Fathimath Hanau <hanau.xd@gmail.com>
2020-05-15 04:09:37 +0530

    Add margin to spinner

--- src/modules/components/ui/spinner/Spinner.js
+++ src/modules/components/ui/spinner/Spinner.js
@@ -3,7 +3,9 @@ import React from 'react';
 import styles from './Spinner.module.css';
 
 const Spinner = () => (
-  <div className={styles.Loader}>Loading...</div>
-)
+  <div style={{ margin: '15% auto' }} className={styles.Loader}>
+    Loading...
+  </div>
+);
 
 export default Spinner;
