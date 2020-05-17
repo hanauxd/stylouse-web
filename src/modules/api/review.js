a3fbcee9ee140c7ce1191664e3ae16affab0585e
Fathimath Hanau <hanau.xd@gmail.com>
2020-05-17 07:40:52 +0530

    Check if user is signed in when getting th review list

--- src/modules/api/review.js
+++ src/modules/api/review.js
@@ -1,13 +1,13 @@
 import { post, get, remove } from './api';
 
 export const onCreateReview = (review, token) => {
-  const endpoint = 'reviews';
+  const endpoint = 'reviews?sort=date,desc';
   return post(endpoint, review, token);
 };
 
-export const onFetchReviewsByProduct = (productId) => {
+export const onFetchReviewsByProduct = (productId, token) => {
   const endpoint = `reviews/product/${productId}?sort=date,desc`;
-  return get(endpoint);
+  return get(endpoint, token);
 };
 
 export const onDeleteReview = (reviewId, token) => {
