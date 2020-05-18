c5db7ac390acca98dc679366ac6bca1915c18a60
Fathimath Hanau <hanau.xd@gmail.com>
2020-05-18 12:45:34 +0530

    Inquiry and reply APIs

--- src/modules/api/inquiry.js
+++ src/modules/api/inquiry.js
@@ -1,6 +1,26 @@
-import { post } from './api';
+import { post, get } from './api';
 
 export const onCreateInquiry = (inquiry, token) => {
   const endpoint = 'inquiries';
   return post(endpoint, inquiry, token);
+};
+
+export const onFetchInquiriesByUser = (token) => {
+  const endpoint = 'inquiries/user';
+  return get(endpoint, token);
+};
+
+export const onMarkReplyRead = (replyIds, token) => {
+  const endpoint = 'replies/read';
+  return post(endpoint, replyIds, token);
+};
+
+export const onFetchAllInquiries = (token) => {
+  const endpoint = 'inquiries/all';
+  return get(endpoint, token);
+};
+
+export const onSubmitReply = (token, reply) => {
+  const endpoint = 'replies';
+  return post(endpoint, reply, token);
 };
