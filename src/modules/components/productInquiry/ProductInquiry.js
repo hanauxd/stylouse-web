57dde607ab04af020fe512dad3ef965ca2b9d40e
Fathimath Hanau <hanau.xd@gmail.com>
2020-05-18 05:32:48 +0530

    Product Inquiry component

---
+++ src/modules/components/productInquiry/ProductInquiry.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import cogoToast from 'cogo-toast';
+
+import { InquiryForm } from '..';
+import { useCustomState } from '../../helpers/hooks';
+import { onCreateInquiry } from '../../api/inquiry';
+
+const ProductInquiry = (props) => {
+  const { productId } = props;
+  const [state, setState] = useCustomState({
+    inquiryDialogOpen: false,
+  });
+
+  const handleInquiryDialogOpen = () => {
+    setState({
+      inquiryDialogOpen: true,
+    });
+  };
+
+  const handleInquiryDialogClose = () => {
+    setState({
+      inquiryDialogOpen: false,
+    });
+  };
+
+  const handleInquirySubmit = async (values) => {
+    try {
+      const inquiry = {
+        productIdOrInquiryId: productId,
+        message: values.inquiry,
+      };
+      const token = props.auth.jwt;
+      const result = await onCreateInquiry(inquiry, token);
+      console.log('[INQUIRY SUBMIT]', result);
+    } catch (error) {
+      if (error.request) {
+        const message = JSON.parse(error.request.response).message;
+        cogoToast.error(message);
+      }
+    }
+    handleInquiryDialogClose();
+  };
+
+  return (
+    <div>
+      <div onClick={handleInquiryDialogOpen}>Have a question?</div>
+      <InquiryForm
+        handleClose={handleInquiryDialogClose}
+        handleInquirySubmit={handleInquirySubmit}
+        open={state.inquiryDialogOpen}
+      />
+    </div>
+  );
+};
+
+const mapPropsToState = (state) => {
+  return {
+    auth: state.auth.auth,
+  };
+};
+
+export default connect(mapPropsToState, null)(ProductInquiry);
